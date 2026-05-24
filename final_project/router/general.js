9232df9eb9f023da336624eb6886c4dd26d90fc8
Melvin Añez <135235911+Danik2310@users.noreply.github.com>
2026-05-24 07:03:55 -0500

    Refactor routes to use async/await syntax

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,6 @@ const axios = require('axios');
 public_users.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-
     if (!username || !password) {
         return res.status(400).json({ message: "Username and password are required" });
     }
@@ -20,29 +19,31 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    new Promise((resolve, reject) => {
-        resolve(books);
-    })
-    .then(books => res.status(200).json(books))
-    .catch(err => res.status(500).json({ message: err }));
+public_users.get('/', async function (req, res) {
+    const getBooks = () => new Promise((resolve) => resolve(books));
+    const result = await getBooks();
+    return res.status(200).json(result);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    new Promise((resolve, reject) => {
+    const getBookByISBN = () => new Promise((resolve, reject) => {
         if (books[isbn]) resolve(books[isbn]);
         else reject("Book not found");
-    })
-    .then(book => res.status(200).json(book))
-    .catch(err => res.status(404).json({ message: err }));
+    });
+    try {
+        const result = await getBookByISBN();
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(404).json({ message: err });
+    }
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author.toLowerCase();
-    new Promise((resolve, reject) => {
+    const getBooksByAuthor = () => new Promise((resolve, reject) => {
         let result = {};
         Object.keys(books).forEach((isbn) => {
             if (books[isbn].author.toLowerCase().includes(author)) {
@@ -51,15 +52,19 @@ public_users.get('/author/:author', function (req, res) {
         });
         if (Object.keys(result).length > 0) resolve(result);
         else reject("No books found by this author");
-    })
-    .then(result => res.status(200).json(result))
-    .catch(err => res.status(404).json({ message: err }));
+    });
+    try {
+        const result = await getBooksByAuthor();
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(404).json({ message: err });
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title.toLowerCase();
-    new Promise((resolve, reject) => {
+    const getBooksByTitle = () => new Promise((resolve, reject) => {
         let result = {};
         Object.keys(books).forEach((isbn) => {
             if (books[isbn].title.toLowerCase().includes(title)) {
@@ -68,9 +73,13 @@ public_users.get('/title/:title', function (req, res) {
         });
         if (Object.keys(result).length > 0) resolve(result);
         else reject("No books found with this title");
-    })
-    .then(result => res.status(200).json(result))
-    .catch(err => res.status(404).json({ message: err }));
+    });
+    try {
+        const result = await getBooksByTitle();
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(404).json({ message: err });
+    }
 });
 
 // Get book review
